576cf7a2d8618f4a2e53c13f9de75c23227e7221
Harshvardhan <44029089+harshvardhan219@users.noreply.github.com>
2020-07-17 12:13:30 +0530

    Changing password

--- email_send.py
+++ email_send.py
@@ -14,6 +14,6 @@ email.set_content(html.substitute({'name': 'TinTin'}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
   smtp.ehlo()
   smtp.starttls()
-  smtp.login('harshvardhan219@gmail.com', 'singh@9000')
+  smtp.login('harshvardhan219@gmail.com', 'password')
   smtp.send_message(email)
   print('all good boss!')
