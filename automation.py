5504825d796b8e55fc8c2da9245d5bbac83634fc
Harshvardhan <44029089+harshvardhan219@users.noreply.github.com>
2020-07-19 15:18:27 +0530

    Using selenium

---
+++ automation.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+chrome_browser = webdriver.Chrome('./chromedriver')
+
+chrome_browser.maximize_window()
+chrome_browser.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
+
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+
+print(show_message_button.get_attribute('innerHTML'))
+
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('I AM EXTRA COOL.')
+
+show_message_button.click()
+
+output_message = chrome_browser.find_element_by_id('display')
